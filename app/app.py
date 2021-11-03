84e4afe9e35ef031d8c1ae7014f54ba0456e6407
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2021-11-04 00:10:43 +0100

    connect with API

--- app/app.py
+++ app/app.py
@@ -1,13 +1,26 @@
 import datetime as datetime
 import time as time
+import calendar as calendar
+import requests as requests
 
-hour = time.localtime()
 # datetime.datetime.now().time()
-print(hour)
 
-while hour != 22:
-    print("Nie ma jeszcze określonej godziny.")
-    print(hour)
-    time.sleep(5)
-else:
-    print(hour)
+payload = {'formatted':'0'}
+
+response = requests.get("https://api.sunrise-sunset.org/json?lat=50.3657432&lng=18.87153251609449", params = payload)
+
+print(response.json())
+
+# while True:
+#     now = datetime.datetime.now()
+#     hour = now.minute
+#     print(now.minute)
+#     if hour == 48:
+#         now = datetime.datetime.now()
+#         hour = now.minute
+#         print("Otwieranie rolety")
+#         time.sleep(1)
+#     else:
+#         print("Nie ma jeszcze określonej godziny.")
+#         time.sleep(1)
+# time.sleep(1)
