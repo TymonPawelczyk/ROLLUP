a2b92982e7471a498be5d68d205aa3ea02ac6fc2
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2021-11-04 13:23:10 +0100

    Odczyt

--- app/app.py
+++ app/app.py
@@ -1,29 +1,31 @@
 import datetime as datetime
 import time as time
-import calendar as calendar
+# import calendar as calendar
 import requests as requests
 import json
+import gpiozero
 
 
 
-payload = {'formatted':'0'}
+payload = {'lat' : '50.3657432' , 'lng' : '18.87153251609449' , 'formatted' : '0'}
+url = 'https://api.sunrise-sunset.org/json'
 
 #Połaczenie z API
-response = requests.post("https://api.sunrise-sunset.org/json?lat=50.3657432&lng=18.87153251609449", params = payload)
+response = requests.get(url, params = payload)
+json_object = response.json()
+sunset = str(json_object['results']['sunrise'])
 
-print(response.json())
+print(sunset)
+# print(response.headers["results"])
 
 #Sprawdzanie godziny
-while True:
-    now = datetime.datetime.now()
-    hour = now.hour
-    print(now.hour)
-    if hour == 0:
-        now = datetime.datetime.now()
-        hour = now.hour
-        print("Otwieranie rolety")
-        time.sleep(3600)
-    else: 
-        print("Nie ma jeszcze określonej godziny.")
-        time.sleep(1)
-    time.sleep(1)
+# while True:
+#     now = datetime.datetime.now()
+#     hour = now.hour
+#     print(now.hour)
+#     if hour == 14:
+#         print("Otwieranie rolety")
+#         time.sleep(3600)
+#     else: 
+#         print("Nie ma jeszcze określonej godziny.")
+#     time.sleep(1)
