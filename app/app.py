bedf372629d9edf522200010463bb745534d7ec1
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2021-11-04 18:13:10 +0100

    Czas

--- app/app.py
+++ app/app.py
@@ -8,28 +8,37 @@ import gpiozero
 
 
 #Połaczenie z API
-payload = {'lat' : '50.3657432' , 'lng' : '18.87153251609449' , 'formatted' : '1'}
+payload = {'lat' : '50.3657432' , 'lng' : '18.87153251609449' , 'formatted' : '0'}
 url = 'https://api.sunrise-sunset.org/json'
 
 #Zapytanie API
 response = requests.get(url, params = payload)
 json_object = response.json()
 
-sunrise = str(json_object['results']['sunrise'])
-sunset = str(json_object['results']['sunset'])
+responce_sunrise = str(json_object['results']['sunrise'])
+responce_sunset = str(json_object['results']['sunset'])
 
-# print(json_object)
+#Formatowanie 
+sunrise = responce_sunrise.split('T')
+sunset = responce_sunset.split('T')
+sunrise = (sunrise[1].partition('+'))
+sunset = sunset[1].partition('+')
+sunrise = sunrise[0]
+sunset = sunset[0]
+
 print(sunrise,sunset)
 
-
 #Sprawdzanie godziny
-# while True:
-#     now = datetime.datetime.now()
-#     hour = now.hour
-#     print(now.hour)
-#     if hour == 14:
-#         print("Otwieranie rolety")
-#         time.sleep(3600)
-#     else: 
-#         print("Nie ma jeszcze określonej godziny.")
-#     time.sleep(1)
+while True:
+    now = datetime.datetime.now()
+    hour = str(now.hour)
+    minute = str(now.minute)
+    seconds = str(now.second)
+    CURRENT_TIME_str = (hour+":"+minute+":"+seconds)
+    print(CURRENT_TIME_str)
+    if CURRENT_TIME_str == "18:11:30":
+        print("Otwieranie rolety")
+        time.sleep(3600)
+    else: 
+        print("Nie ma jeszcze określonej godziny.")
+    time.sleep(1)
